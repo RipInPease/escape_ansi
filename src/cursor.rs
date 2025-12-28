a8d6348283a7d5c69d01d718c4003cde72deaeb0
RipInPease <svorrevfjelde@gmail.com>
2025-12-28 18:08:57 +0100

    write! macro

--- src/cursor.rs
+++ src/cursor.rs
@@ -18,19 +18,17 @@ pub struct MoveHome;
 
 impl<W: Write> Command<W> for MoveHome {
     fn write_ansi(&self, w: &mut W) -> IOResult<()> {
-        let mut bytes = Vec::with_capacity(2);
-        bytes.extend_from_slice(ESC);
-        bytes.extend_from_slice(CSI);
-        bytes.extend_from_slice("H".as_bytes());
-
-        w.write(&bytes)?;
-        Ok(())
+        crate::write!(
+            w,
+            ESC,
+            "[H".as_bytes()
+        )
     }
 }
 
 
 
-/// Moves cursor to desires (column, row)
+/// Moves cursor to desired (column, row)
 /// 
 #[derive(Debug, Clone)]
 pub struct MoveTo(pub u16, pub u16);
@@ -38,22 +36,10 @@ pub struct MoveTo(pub u16, pub u16);
 
 impl<W: Write> Command<W> for MoveTo {
     fn write_ansi(&self, w: &mut W) -> IOResult<()> {
-        let mut bytes = Vec::with_capacity(10);
-        bytes.extend_from_slice(ESC);
-        bytes.extend_from_slice(CSI);
-
-        let column = &self.1.to_string();
-        bytes.extend_from_slice(column.as_bytes());
-
-        bytes.extend_from_slice(SEMI_COLON);
-
-        let row = &self.0.to_string();
-        bytes.extend_from_slice(row.as_bytes());
-
-        bytes.extend_from_slice("H".as_bytes());
-
-        w.write(&bytes)?;
-
-        Ok(())
+        write!(
+            w,
+            ESC,
+            format!("[{};{}H", self.1, self.0).as_bytes()
+        )
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
+use std::io::Write;
+use std::io::Result as IOResult;
+
+
+
+
+/// Write all u8 slices to a writer
+/// 
+#[macro_export]
+macro_rules! write {
+    ($w:ident, $($b:expr),*) => {{
+        let mut bytes = Vec::new();
+        $(bytes.extend_from_slice($b);)*
+        $w.write(&bytes)?;
+        Ok(())
+    }};
+}
+
+
 /// Commands and such for the cursor
 /// 
 pub mod cursor;
-
-
-use std::io::Write;
-use std::io::Result as IOResult;
 
 
 /// A command that can be written to a terminal
